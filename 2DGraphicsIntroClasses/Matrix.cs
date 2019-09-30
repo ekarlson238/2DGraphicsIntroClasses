589ea2fb870762d61dcb27548ec9c32663e109c7
ekarlson238 <ekarlson238@gmail.com>
2019-09-29 22:34:12 -0500

    updating matrix

--- 2DGraphicsIntroClasses/Matrix.cs
+++ 2DGraphicsIntroClasses/Matrix.cs
@@ -1,13 +1,19 @@
 using System;
 
-
+/// <summary>
+/// a 2x2 matrix
+/// </summary>
 public struct Matrix2
 {
+    //the width and height of the matrix
     public const int Len = 2;
 
+    //row 0 colum 0, row 0 colum 1
     public float R0C0, R0C1;
+    //row 1 colum 0, row 1 colum 1
     public float R1C0, R1C1;
 
+    //a position in the matrix [row position, column position]
     public float this[int row, int column]
     {
         get
@@ -59,14 +65,22 @@ public struct Matrix2
     }
 }
 
+/// <summary>
+/// a 3x3 matrix
+/// </summary>
 public struct Matrix3
 {
+    //the width and height of the matrix
     public const int Len = 3;
 
+    //row 0 colum 0, row 0 colum 1, row 0 colum 2
     public float R0C0, R0C1, R0C2;
+    //row 1 colum 0, row 1 colum 1, row 1 colum 2
     public float R1C0, R1C1, R1C2;
+    //row 2 colum 0, row 2 colum 1, row 2 colum 2
     public float R2C0, R2C1, R2C2;
 
+    //gets or sets a position in the matrix [row position, column position]
     public float this[int row, int column]
     {
         get
@@ -139,6 +153,7 @@ public struct Matrix3
         }
     }
 
+    //flips the rows and columns
     public Matrix3 Transpose()
     {
         return new Matrix3
@@ -155,6 +170,11 @@ public struct Matrix3
         };
     }
 
+    /// <summary>
+    /// places a vector 3 in a column
+    /// </summary>
+    /// <param name="col">the column the vector 3 is being placed in</param>
+    /// <param name="v">the vector 3</param>
     public void SetColumn(int col, Vec3f v)
     {
         this[0, col] = v.x;
@@ -162,6 +182,11 @@ public struct Matrix3
         this[2, col] = v.z;
     }
 
+    /// <summary>
+    /// places a vector 3 in a row
+    /// </summary>
+    /// <param name="row">the  row the vector 3 is being placed in</param>
+    /// <param name="v">the vector 3</param>
     public void SetRow(int row, Vec3f v)
     {
         this[row, 0] = v.x;
@@ -169,6 +194,7 @@ public struct Matrix3
         this[row, 2] = v.z;
     }
 
+    //uses the string builder to convert the matix into a string (does the rows first)
     public override string ToString()
     {
         var sb = new System.Text.StringBuilder();
@@ -182,15 +208,24 @@ public struct Matrix3
     }
 }
 
+/// <summary>
+/// a 4x4 matrix
+/// </summary>
 public struct Matrix4
 {
+    //the width and height of the matrix
     public const int Len = 4;
 
+    //row 0 colum 0, row 0 colum 1, row 0 colum 2, row 0 colum 3
     public float R0C0, R0C1, R0C2, R0C3;
+    //row 1 colum 0, row 1 colum 1, row 1 colum 2, row 1 colum 3
     public float R1C0, R1C1, R1C2, R1C3;
+    //row 2 colum 0, row 2 colum 1, row 2 colum 2, row 2 colum 3
     public float R2C0, R2C1, R2C2, R2C3;
+    //row 3 colum 0, row 3 colum 1, row 3 colum 2, row 3 colum 3
     public float R3C0, R3C1, R3C2, R3C3;
 
+    // gets or sets a position in the matrix [row position, column position]
     public float this[int row, int column]
     {
         get
@@ -289,6 +324,10 @@ public struct Matrix4
         }
     }
 
+    /// <summary>
+    /// flips the rows and colums
+    /// </summary>
+    /// <returns>the new flipped matrix</returns>
     public Matrix4 Transpose()
     {
         return new Matrix4
@@ -312,17 +351,33 @@ public struct Matrix4
         };
     }
 
-
+    /// <summary>
+    /// makes a reset matrix
+    /// </summary>
+    /// <returns>the reset matrix</returns>
     public static Matrix4 Identity()
     {
         return new Matrix4 { R0C0 = 1, R1C1 = 1, R2C2 = 1, R3C3 = 1 };
     }
 
+    /// <summary>
+    /// makes a matrix with a scale of 'scale'
+    /// </summary>
+    /// <param name="scale">the scale</param>
+    /// <returns>the created matrix</returns>
     public static Matrix4 Zoom(float scale)
     {
         return new Matrix4 { R0C0 = scale, R1C1 = scale, R2C2 = scale, R3C3 = scale };
     }
 
+    /// <summary>
+    /// Determines the Rotation of the object in 3D space by comparing it's orientation to the world space's 
+    /// (x, y), (y, z), and (x, z) coordinate grids.
+    /// (R[0, 0] and R[1, 1] each == (x, y) && (y, x) respectively)
+    /// http://setosa.io/ev/sine-and-cosine/
+    /// </summary>
+    /// <param name="angle">the angle in degrees</param>
+    /// <returns>a rotation</returns>
     public static Matrix4 RotationZ(float angle)
     {
         var cosangle = (float)Math.Cos(angle);
@@ -336,7 +391,8 @@ public struct Matrix4
         return R;
     }
 
-
+    //multiplies the 2 matrixs by multiplying each of their positions
+    //returns a new matrix 4 created from the results
     public static Matrix4 operator *(Matrix4 l, Matrix4 r)
     {
         var result = new Matrix4();
@@ -354,6 +410,7 @@ public struct Matrix4
         return result;
     }
 
+    //uses the string builder to convert the matix into a string (does the rows first)
     public override string ToString()
     {
         var sb = new System.Text.StringBuilder();
@@ -394,7 +451,8 @@ static class MatrixHelpers
 		return cofactor;
 	}
 
-	public static Matrix4 Inverse (Matrix4 m)
+    //it finds the inverse https://www.mathwords.com/i/inverse_of_a_matrix.htm
+    public static Matrix4 Inverse (Matrix4 m)
 	{
 		// where Inverse(m) = Transpose(Cofactor(m)) / det(m)
 		var cofactor = Cofactor (m);
@@ -414,7 +472,8 @@ static class MatrixHelpers
 		return cofactor;
 	}
 
-	public static Matrix3 Inverse (Matrix3 m)
+    //it finds the inverse https://www.mathwords.com/i/inverse_of_a_matrix.htm
+    public static Matrix3 Inverse (Matrix3 m)
 	{
 		// where Inverse(m) = Transpose(Cofactor(m)) / det(m)
 		var cofactor = Cofactor (m);
@@ -434,6 +493,11 @@ static class MatrixHelpers
 		return cofactor;
 	}
 
+    /// <summary>
+    /// creates a matrix 4 of cofactors for each position in the given matrix 4
+    /// </summary>
+    /// <param name="m">the given matrix</param>
+    /// <returns>the matrix 4  of cofactors</returns>
 	static Matrix4 Cofactor (Matrix4 m)
 	{
 		var r = new Matrix4 ();
@@ -444,6 +508,11 @@ static class MatrixHelpers
 		return r;
 	}
 
+    /// <summary>
+    /// creates a matrix 3 of cofactors for each position in the given matrix 3
+    /// </summary>
+    /// <param name="m">the given matrix</param>
+    /// <returns>the matrix 3 of cofactors</returns>
 	static Matrix3 Cofactor (Matrix3 m)
 	{
 		var r = new Matrix3 ();
@@ -454,18 +523,27 @@ static class MatrixHelpers
 		return r;
 	}
 
-	static float Cofactor (Matrix4 m, int row, int col)
+    //returns the cofactor for m[row, col]
+    static float Cofactor (Matrix4 m, int row, int col)
 	{
 		int sign = ((row + col) % 2 == 0) ? 1 : -1;
 		return Det (Minor (m, row, col)) * sign;
 	}
 
+    //returns the cofactor for m[row, col]
 	static float Cofactor (Matrix3 m, int row, int col)
 	{
 		int sign = ((row + col) % 2 == 0) ? 1 : -1;
 		return Det (Minor (m, row, col)) * sign;
 	}
 
+    /// <summary>
+    /// converts a matrix 4 into a matrix 3 by not including the given row and column
+    /// </summary>
+    /// <param name="m">the matrix 4 to convert</param>
+    /// <param name="row">the row to ignore</param>
+    /// <param name="col">the column to ignore</param>
+    /// <returns>the new matrix 3</returns>
 	static Matrix3 Minor (Matrix4 m, int row, int col)
 	{
 		var minor = new Matrix3 ();
@@ -479,6 +557,13 @@ static class MatrixHelpers
 		return minor;
 	}
 
+    /// <summary>
+    /// converts a matrix 3 into a matrix 2 by not including the given row and column
+    /// </summary>
+    /// <param name="m">the matrix 3 to convert</param>
+    /// <param name="row">the row to ignore</param>
+    /// <param name="col">the column to ignore</param>
+    /// <returns>the new matrix 2</returns>
 	static Matrix2 Minor (Matrix3 m, int row, int col)
 	{
 		var minor = new Matrix2 ();
@@ -492,6 +577,7 @@ static class MatrixHelpers
 		return minor;
 	}
 
+    //Calculates the determinant of a 3x3 matrix https://en.m.wikipedia.org/wiki/Determinant
 	static float Det (Matrix3 m)
 	{
 		float det = 0;
@@ -501,11 +587,13 @@ static class MatrixHelpers
 		return det;
 	}
 
-	static float Det (Matrix2 m)
+    //Calculates the determinant of a 2x2 matrix https://en.m.wikipedia.org/wiki/Determinant
+    static float Det (Matrix2 m)
 	{
 		return m [0, 0] * m [1, 1] - m [0, 1] * m [1, 0];
 	}
 
+    //multiplies the vector 3 by everything in the row relative to their cordinate position (x=0,y=1,z=2)
 	public static Vec3f Mult (Matrix3 m, Vec3f v)
 	{
 		return new Vec3f {
@@ -515,7 +603,8 @@ static class MatrixHelpers
 		};
 	}
 
-	public static Vec4f Mult (Matrix4 m, Vec4f v)
+    //multiplies the vector 4 by everything in the row relative to their cordinate position (x=0,y=1,z=2,h=3)
+    public static Vec4f Mult (Matrix4 m, Vec4f v)
 	{
 		return new Vec4f {
 			x = m.R0C0*v.x + m.R0C1*v.y + m.R0C2*v.z + m.R0C3*v.h,
